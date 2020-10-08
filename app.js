12a936860ee28a4a5e7d3d85bb20d4f9274a38dd
dtapia4206 <dtapia4206@uta.com.ec>
2020-10-07 19:24:26 -0500

    Pruebas

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app=express();
+const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Cabeceras
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Controll-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,DELETE');
-    res.header('Allow','GET,POST,OPTIONS,PUT,DELETE');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Controll-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
+    res.header('Allow', 'GET,POST,OPTIONS,PUT,DELETE');
     next();
 });
 
 //Rutas
-app.get('*',(req,res)=>{
-    res.status(200).send({message:"Bienvenido al servidor NodeJS"});
+app.get('*', (req, res) => {
+    res.status(200).send({ message: "Primeros cambios en el repositorio por Daniel Tapia" });
 });
 
 
-module.exports=app;
+
+
+module.exports = app;

31d8bd2574513f1d9d5839232955a4e7c9a399ea
Diego Yanez <diegoyanez13@hotmail.com>
2020-10-18 21:47:42 -0500

    Creando ruta Login

--- server/controllers/usuarios.js
+++ server/controllers/usuarios.js
@@ -10,6 +10,28 @@ function create(req, res) {
         })
 }
 
+function login(req, res) {
+    usuarios.findOne({
+            where: {
+                usuario: req.body.usuario,
+                password: req.body.password
+            }
+        })
+        .then(usuario => {
+            if (usuario) {
+                res.status(200).send({ usuario });
+            } else {
+                res.status(401).send({ message: "Acceso no autorizado" })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: "Ocurrió un error al buscar el Usuario." });
+        })
+
+}
+
+
 module.exports = {
-    create
+    create,
+    login
 }
--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -2,4 +2,5 @@ const usuariosController = require('../controllers').usuarios;
 
 module.exports = (app) => {
     app.post('/api/usuario', usuariosController.create);
+    app.post('/api/login', usuariosController.login);
 }

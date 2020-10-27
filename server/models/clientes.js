d5fe1d7ad72e842178256567ec66633203799b16
Diego Yanez <diegoyanez13@hotmail.com>
2020-10-26 21:19:57 -0500

    Se modifica modelo clientes

--- server/models/clientes.js
+++ server/models/clientes.js
@@ -1,15 +1,15 @@
-module.exports=(sequelize,DataTypes)=>{
-    const clientes=sequelize.define('clientes',{
+module.exports = (sequelize, DataTypes) => {
+    const clientes = sequelize.define('clientes', {
         idempresa: DataTypes.STRING,
         idcliente: {
-            primaryKey:true,
-            type:DataTypes.STRING,
+            primaryKey: true,
+            type: DataTypes.STRING,
         },
         idencliente: DataTypes.STRING,
         ruccicliente: DataTypes.STRING,
-        nocliente: DataTypes.STRING,
+        nomcliente: DataTypes.STRING,
         dircliente: DataTypes.STRING,
-        teflcliente: DataTypes.STRING,
+        telfcliente: DataTypes.STRING,
         emailcliente: DataTypes.STRING,
         crepor: DataTypes.INTEGER,
         modpor: DataTypes.INTEGER,

b1d5f7bfbbf1707827fd07da09198ddd17ed91d5
Daniel Tapia <alejodanny@gmail.com>
2020-11-07 01:42:54 -0500

    token obligatorio

--- server/routes/clientes.js
+++ server/routes/clientes.js
@@ -1,7 +1,8 @@
 const clientesController = require('../controllers').clientes;
+const md_auth = require('../authenticated/authenticated');
 
 module.exports = (app) => {
-    app.post('/api/cliente', clientesController.create);
-    app.put('/api/cliente/:id', clientesController.update);
-    app.get('/api/clientes/:id', clientesController.getAll);
+    app.post('/api/cliente', md_auth.auth, clientesController.create);
+    app.put('/api/cliente/:id', md_auth.auth, clientesController.update);
+    app.get('/api/clientes/:id', md_auth.auth, clientesController.getAll);
 }

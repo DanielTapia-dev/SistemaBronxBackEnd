3041d93cd28264a81f727deac21dc287dfb232ba
Daniel Tapia <alejodanny@gmail.com>
2020-12-05 10:26:16 -0500

    ¨Ultimos cambios

--- server/routes/parformapago.js
+++ server/routes/parformapago.js
@@ -1,7 +1,8 @@
 const parformapagoController = require('../controllers').parformapago;
+const md_auth = require('../authenticated/authenticated');
 
 module.exports = (app) => {
-    app.post('/api/parformapago', parformapagoController.create);
-    app.put('/api/parformapago/:id', parformapagoController.update);
-    app.get('/api/parformapagos', parformapagoController.getAll);
+    app.post('/api/parformapago', md_auth.auth, parformapagoController.create);
+    app.put('/api/parformapago/:id', md_auth.auth, parformapagoController.update);
+    app.get('/api/parformapagos/:id', md_auth.auth, parformapagoController.getAll);
 }

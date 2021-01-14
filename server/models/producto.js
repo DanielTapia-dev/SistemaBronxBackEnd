abeaa686d42ebac3263012df5d027132e2437701
Diego Yanez <diegoyanez13@hotmail.com>
2021-01-13 21:18:28 -0500

    se agrega linea para que funcione modelo producto sin la s

--- server/models/producto.js
+++ server/models/producto.js
@@ -19,6 +19,8 @@ module.exports = (sequelize, DataTypes) => {
         activo: DataTypes.BOOLEAN,
         createdAt: DataTypes.DATE,
         updatedAt: DataTypes.DATE
+    }, {
+        freezeTableName: true
     });
 
     return producto;

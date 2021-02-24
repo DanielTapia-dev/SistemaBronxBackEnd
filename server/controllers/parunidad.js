c0be2081c44c500958b4a614feac205fbe1c485f
Diego Yanez <diegoyanez13@hotmail.com>
2021-02-24 15:58:39 -0500

    Ajustes a route parunidad

--- server/controllers/parunidad.js
+++ server/controllers/parunidad.js
@@ -1,48 +1,112 @@
-const parunidad = require('../models').parunidad;
+const parunidad = require("../models").parunidad;
 
 function create(req, res) {
-    parunidad.create(req.body).then(parunidad => {
-        res.status(200).send({ parunidad });
-    }).catch(err => {
-        res.status(500).send({ err });
+  parunidad
+    .create(req.body)
+    .then((parunidad) => {
+      res.status(200).send({ parunidad });
     })
+    .catch((err) => {
+      res.status(500).send({ err });
+    });
 }
 
 function update(req, res) {
-    var id = req.params.id;
-    var body = req.body;
-
-    parunidad.findOne({
-            where: {
-                idunidad: id
-            }
+  var id = req.params.id;
+  var body = req.body;
+  parunidad
+    .findOne({
+      where: {
+        idunidad: id,
+      },
+    })
+    .then((unidad) => {
+      unidad
+        .update(body)
+        .then(() => {
+          res.status(200).send({ unidad });
         })
-        .then(unidad => {
-            unidad.update(body)
-                .then(() => {
-                    res.status(200).send({ unidad });
-                })
-                .catch(erro => {
-                    res.status(500).send({ message: "Ocurrio un error al actualizar la unidad" });
-                })
-        })
-        .catch(err => {
-            res.status(500).send({ message: "Ocurrio un error actualizar la unidad - No se encontro la unidad" });
+        .catch((erro) => {
+          res
+            .status(500)
+            .send({ message: "Ocurrio un error al actualizar la unidad" });
         });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Ocurrio un error al actualizar la unidad" });
+    });
+}
+
+function getOne(req, res) {
+  var idparunidad = req.params.id;
+  parunidad
+    .findOne({
+      where: {
+        idunidad: idparunidad,
+      },
+    })
+    .then((parunidad) => {
+      res.status(200).send(parunidad);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Ocurrió un error al buscar la unidad." + err });
+    });
 }
 
 function getAll(req, res) {
-    parunidad.findAll()
-        .then(parunidad => {
-            res.status(200).send({ parunidad });
+  var idEmpresa = req.params.id;
+  parunidad
+    .findAll({
+      where: {
+        idempresa: idEmpresa,
+      },
+    })
+    .then((parunidad) => {
+      res.status(200).send({ parunidad });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Ocurrio un error al buscar las unidades" });
+    });
+}
+
+function borrar(req, res) {
+  var id = req.params.id;
+  var body = req.body;
+  parunidad
+    .findOne({
+      where: {
+        idunidad: id,
+      },
+    })
+    .then((unidad) => {
+      unidad
+        .destroy(body)
+        .then(() => {
+          res.status(200).send({ message: "Unidad eliminada" });
         })
-        .catch(err => {
-            res.status(500).send({ message: "Ocurrio un error al buscar las unidades" });
-        })
+        .catch((erro) => {
+          res
+            .status(500)
+            .send({ message: "Ocurrio un error al borrar la unidad" });
+        });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Ocurrio un error al borrar la unidad" });
+    });
 }
 
 module.exports = {
-    create,
-    update,
-    getAll
+  create,
+  update,
+  getAll,
+  borrar,
+  getOne
 }
--- server/routes/parunidad.js
+++ server/routes/parunidad.js
@@ -3,6 +3,10 @@ const md_auth = require('../authenticated/authenticated');
 
 module.exports = (app) => {
     app.post('/api/parunidad', md_auth.auth, parunidadController.create);
+    app.get('/api/parunidad/:id', md_auth.auth, parunidadController.getOne);
     app.put('/api/parunidad/:id', md_auth.auth, parunidadController.update);
-    app.get('/api/parunidades', md_auth.auth, parunidadController.getAll);
+    app.get('/api/parunidades/:id', md_auth.auth, parunidadController.getAll);
+    app.delete('/api/parunidad/delete/:id', md_auth.auth, parunidadController.borrar);
 }
+
+    

b2e8d1f5dc13fae5753668d947cd852c9c099b0f
Diego Yanez <diegoyanez13@hotmail.com>
2021-03-26 00:08:26 -0500

    Aumento de servicios para sucursales

--- server/routes/parsucursal.js
+++ server/routes/parsucursal.js
@@ -4,5 +4,7 @@ const md_auth = require('../authenticated/authenticated');
 module.exports = (app) => {
     app.post('/api/sucursal', md_auth.auth, sucursalController.create);
     app.put('/api/sucursal/:id', md_auth.auth, sucursalController.update);
-    app.get('/api/sucursales', md_auth.auth, sucursalController.getAll);
+    app.get('/api/sucursales/:id', md_auth.auth, sucursalController.getAll);
+    app.get('/api/sucursal/:id', md_auth.auth, sucursalController.getOne);
+    app.delete('/api/sucursal/delete/:id', md_auth.auth, sucursalController.borrar);
 }

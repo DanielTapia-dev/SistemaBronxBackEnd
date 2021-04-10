94fe1756b2b824d9a563c1c047f423a47150d147
Diego Yanez <diegoyanez13@hotmail.com>
2021-04-09 23:17:11 -0500

    Se agrega servicios a usuarios

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -3,7 +3,9 @@ const md_auth = require('../controllers/usuarios');
 
 module.exports = (app) => {
     app.post('/api/usuario', md_auth.auth, usuariosController.create);
+    app.put('/api/usuario/:id', md_auth.auth, usuariosController.update);
     app.post('/api/login', usuariosController.login);
-    app.get('/api/usuarios', md_auth.auth, usuariosController.getAll);
+    app.get('/api/usuarios/:id', md_auth.auth, usuariosController.getAll);
     app.get('/api/comprobar', md_auth.auth);
+    app.delete('/api/usuario/delete/:id', md_auth.auth, usuariosController.borrar);
 }

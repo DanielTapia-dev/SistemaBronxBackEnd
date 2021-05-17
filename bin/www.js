ca7551db1453602dbfdd0c0568922d612b12b5ce
Daniel <d.tapialopez1@gmail.com>
2021-05-17 12:59:05 -0500

    ¨Cambio de www.js a produccion

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const app = require('../app');
-const port = parseInt(process.env.port, 10) || 8010;
+const port = parseInt(process.env.port, 10) || 5000;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port);
+server.listen(process.env.PORT || 8010, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});

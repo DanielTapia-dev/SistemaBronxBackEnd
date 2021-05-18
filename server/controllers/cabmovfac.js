acb652b509609131e8b5a9502013d0ed85c35492
Daniel <d.tapialopez1@gmail.com>
2021-05-18 04:09:25 -0500

    ssl activated

--- server/controllers/cabmovfac.js
+++ server/controllers/cabmovfac.js
@@ -77,14 +77,15 @@ cron.schedule('0 0 * * *', () => {
 //********FIN DE PROCESO 12PM */
 /* const { sequelize } = require('sequelize');
 const producto = require("../models").producto; */
-
+const fss = require('fs');
 const config = {
     user: 'orbnggveukawgo',
     host: 'ec2-107-20-153-39.compute-1.amazonaws.com',
     password: '6e78fad641772ae6d9be773d3f5fe56e3e47119ac747fb18e6f4bc22f0db44ba',
     database: 'd2o06sd0oim7ud',
-    ssl: true,
-    rejectUnauthorized: false
+    ssl: {
+        rejectUnauthorized: false
+    },
 };
 
 function EnviarFacturaElectronica() {

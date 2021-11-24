5a9c05c04f324fbcd65bc8a4d5bcaed068939e7e
Daniel <d.tapialopez1@gmail.com>
2021-11-23 22:01:15 -0500

    ¨Primera subida a produccion

--- server/config/configpg.js
+++ server/config/configpg.js
@@ -1,8 +1,8 @@
 const config = {
-    user: "postgres",
-    host: "localhost",
-    password: "dannyalejo7123tapia",
-    database: "innova",
+  user: "myddjzqqxebpvr",
+  host: "ec2-44-196-71-136.compute-1.amazonaws.com",
+  password: "cabf00ac3c194adcbbd4fe271afabbea0f4ee0d3492b9c616e8e6b14edaad92e",
+  database: "dfjjb408kt3gkv",
 };
 /* const config = {
     user: "orbnggveukawgo",
@@ -15,5 +15,5 @@ const config = {
 }; */
 
 module.exports = {
-    config,
+  config,
 };

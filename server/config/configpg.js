535969d892ff69ce725569b406acc432a570bb0a
Daniel <d.tapialopez1@gmail.com>
2022-12-14 00:22:21 -0500

    coneccion de produccion

--- server/config/configpg.js
+++ server/config/configpg.js
@@ -6,10 +6,10 @@
 }; */
 
 const config = {
-  user: "cpses_keaioww93y",
-  host: "ec2-44-196-71-136.compute-1.amazonaws.com",
-  password: "GYDkFq4qObRWwkc0XzcnnRfO7uv8Eaf7",
-  database: "kengisof_thebronx",
+  user: "yagtgymbcvesov",
+  host: "ec2-54-157-79-121.compute-1.amazonaws.com",
+  password: "8b217138a90568e116f31050328001a6353c765e962319c618ea060d4ba70812",
+  database: "d5dt248035saq7",
   ssl: {
     rejectUnauthorized: false,
   },
